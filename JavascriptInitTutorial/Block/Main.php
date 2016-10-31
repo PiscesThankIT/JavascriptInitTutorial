9a29683b4eb838e6d1c6f37ea85d99c24af111a1
PiscesThankIT <wuqiong1518@gmail.com>
2016-10-31 17:19:47 +0800

    commit

---
+++ JavascriptInitTutorial/Block/Main.php
@@ -0,0 +1,12 @@
+<?php
+namespace Pulsestorm\JavascriptInitTutorial\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Main extends Template
+{
+	protected function _prepareLayout()
+	{
+	    
+	} 
+}
---
+++ JavascriptInitTutorial/view/frontend/web/example.js
@@ -0,0 +1,36 @@
+//File: app/code/Pulsestorm/JavascriptInitTutorial/view/frontend/web/example.js
+// define([], function(){
+//     alert("A simple RequireJS module");
+//     return {};    
+// });
+
+// define([], function () {
+//     var mageJsComponent = function()
+//     {
+//         alert("A simple magento component.");
+//     };
+
+//     return mageJsComponent;
+// });
+
+// define([], function () {
+//     var mageJsComponent = function(config)
+//     {
+//         alert("Look in your browser's console");
+//         console.log(config);
+//         //alert(config);
+//     };
+
+//     return mageJsComponent;
+// });
+
+define([], function () {
+    var mageJsComponent = function(config, node)
+    {       
+        console.log(config);
+        console.log(node);
+        //alert(config);
+    };
+
+    return mageJsComponent;
+});
